1a7ced3876af978054cc279ccba556009f0275fc
Matthias_F <fellmatt007@gmail.com>
2024-04-03 16:30:35 +0200

    Ex_JavaScript

--- Frontend/HTML_CSS_JS/Java_Script/Conditions_&_Boucles/script.js
+++ Frontend/HTML_CSS_JS/Java_Script/Conditions_&_Boucles/script.js
@@ -18,21 +18,47 @@
 //Exercice 3
 
 function deviner(){
-    const result = parseInt(Math.random() * (100 - 1) +1);
-    let nombre = null;
+    // const result = Math.floor((Math.random() * 100) +1); //probleme 
+    const result = parseInt((Math.random() * 100) +1);
     console.log(result);
+    let nombre = parseInt(prompt("Entrez un nombre entre 1 et 100 :"));
     while(nombre != result){
-        nombre = parseInt(prompt("1 Entrez un nombre entre 1 et 100 :"));
         if(nombre < result){
-            alert("Trop petit");
+            prompt("Trop petit");
         }
         if(nombre > result){
-            alert("Trop grand");
+            prompt("Trop grand");
         }
-
+        if(nombre == result){
+            alert("GG t'as gagné !");
+        }
     }
-    alert("GG t'as gagné !");
 }
 deviner();
 
 //Exercice 4
+
+// function calendrier(mois){
+
+//     switch (mois){
+//         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+//             console.log("il y a 31 jours dans ce mois");
+//             break;
+
+//         case 4: case 6: case 9: case 11:
+//             console.log("il y a 30 jours dans ce mois");
+//             break;
+        
+//         case 2:
+//             const annee = (new Date().getFullYear())%4;
+//             // console.log(annee);
+//             if(annee == 0){
+//                 console.log("il y a 29 jours dans ce mois cette année-ci");
+//             }
+//             else{
+//                 console.log("il y a 28 jours dans ce mois cette année-ci");
+//             }
+//             break;
+//     }
+// }
+// calendrier(parseInt(prompt("Entrez un mois :")));
---
+++ Frontend/HTML_CSS_JS/Java_Script/Fonctions/script.js
@@ -0,0 +1,13 @@
+//Exercice 1
+function sumOfEvenNumbers(tab){
+    return tab.filter(number => number % 2 === 0);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(sumOfEvenNumbers(numbers));
+
+//Exercice 2
+
+//Exercice 3
+
+//Exercice 4
