24d6cc47d82a0f09f449ac491aecacd6fb0351d1
Matthias_F <fellmatt007@gmail.com>
2024-04-04 11:57:21 +0200

    Ex_JavaScript

--- Frontend/HTML_CSS_JS/Java_Script/Conditions_&_Boucles/script.js
+++ Frontend/HTML_CSS_JS/Java_Script/Conditions_&_Boucles/script.js
@@ -1,64 +1,61 @@
-// //Exercice 1
-// function Age(age){
-//     if(age<18){console.log("Mineur");}
-//     else{console.log("Majeur");}
-// }
-// Age(prompt("Entrez un âge : "));
+//Exercice 1
+function Age(age){
+    if(age<18){console.log("Mineur");}
+    else{console.log("Majeur");}
+}
+Age(prompt("Entrez un âge : "));
 
-// //Exercice 2
-// function pair(){
-//     for(let i=0; i<=20; i++){
-//         if(i%2 == false){
-//             console.log(i);
-//         }
-//     }
-// }
-// pair();
+//Exercice 2
+function pair(){
+    for(let i=0; i<=20; i++){
+        if(i%2 == false){
+            console.log(i);
+        }
+    }
+}
+pair();
 
 //Exercice 3
-
 function deviner(){
-    // const result = Math.floor((Math.random() * 100) +1); //probleme 
-    const result = parseInt((Math.random() * 100) +1);
-    console.log(result);
-    let nombre = parseInt(prompt("Entrez un nombre entre 1 et 100 :"));
+    const result = Math.floor((Math.random() * 100) +1); 
+    console.log(result); //N'affiche pas le console.log ?? 
+    let nombre = Math.floor(prompt("Entrez un nombre entre 1 et 100 :"));
     while(nombre != result){
         if(nombre < result){
-            prompt("Trop petit");
+            nombre = Math.floor(prompt("Trop petit"));
         }
         if(nombre > result){
-            prompt("Trop grand");
+            nombre = Math.floor(prompt("Trop grand"));
         }
         if(nombre == result){
-            alert("GG t'as gagné !");
+            nombre = alert("GG t'as gagné !");
         }
     }
 }
 deviner();
 
 //Exercice 4
+function calendrier(mois){
 
-// function calendrier(mois){
+    switch (mois){
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            console.log("il y a 31 jours dans ce mois");
+            break;
 
-//     switch (mois){
-//         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-//             console.log("il y a 31 jours dans ce mois");
-//             break;
-
-//         case 4: case 6: case 9: case 11:
-//             console.log("il y a 30 jours dans ce mois");
-//             break;
+        case 4: case 6: case 9: case 11:
+            console.log("il y a 30 jours dans ce mois");
+            break;
         
-//         case 2:
-//             const annee = (new Date().getFullYear())%4;
-//             // console.log(annee);
-//             if(annee == 0){
-//                 console.log("il y a 29 jours dans ce mois cette année-ci");
-//             }
-//             else{
-//                 console.log("il y a 28 jours dans ce mois cette année-ci");
-//             }
-//             break;
-//     }
-// }
-// calendrier(parseInt(prompt("Entrez un mois :")));
+        case 2:
+            const annee = (new Date().getFullYear())%4;
+            // console.log(annee);
+            if(annee == 0){
+                console.log("il y a 29 jours dans ce mois cette année-ci");
+            }
+            else{
+                console.log("il y a 28 jours dans ce mois cette année-ci");
+            }
+            break;
+    }
+}
+calendrier(Math.floor(prompt("Entrez un mois (1 à 12) :")));
--- Frontend/HTML_CSS_JS/Java_Script/Fonctions/script.js
+++ Frontend/HTML_CSS_JS/Java_Script/Fonctions/script.js
@@ -1,13 +1,35 @@
 //Exercice 1
-function sumOfEvenNumbers(tab){
-    return tab.filter(number => number % 2 === 0);
-}
+// function sumOfEvenNumbers(tab){
+//     return tab.filter(number => number % 2 === 0);
+// }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(sumOfEvenNumbers(numbers));
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(sumOfEvenNumbers(numbers));
 
 //Exercice 2
+// function mergeArrays(tab1, tab2){
+//     // console.log(tab1 + " / " +tab1.length);
+//     // console.log(tab2 + " / " +tab2.length);
+//     for(let i=0; i<tab2.length; i++){
+//         tab1.splice(tab1.length , 0, tab2[i]);
+//     }
+//     return tab1;
+// }
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// console.log(mergeArrays(arr1, arr2)); // Résultat attendu : [1, 2, 3, 4, 5, 6]
+
 
 //Exercice 3
+function removeDuplicates(tab){
+    const filtered = tab.filter((element, index) => { //filter((element, index)) : parcours un tableau element par element, index permet d'obtenir sa position dans le tableau
+        return tab.indexOf(element) === index; //indexOf(element) : retourn l'index de la premiere occurence de l'element
+    });
+}
+const fruits = ['Apple', 'Banana', 'Apple', 'Orange', 'Banana', 'Apple'];
+console.log("Filtré : " + removeDuplicates(fruits)); // Résultat attendu : ['Apple', 'Banana', 'Orange']
 
 //Exercice 4
+
+//Exercice 5
