16835e8d4912f3deaaec432dfb1066669182e197
Matthias Fellon <fellmatt007@gmail.com>
2024-04-16 08:15:41 +0200

    Fetch et API

--- Frontend/HTML_CSS_JS/Java_Script/Fetch/script.js
+++ Frontend/HTML_CSS_JS/Java_Script/Fetch/script.js
@@ -1,24 +1,30 @@
-const pokemon = document.querySelector(".pokemon");
-const creerDiv = document.createElement('div');
-const creerText = document.createElement('p');
-const creerImage = document.createElement('img');
 
-creerDiv.className = "conteneur";
-creerDiv.appendChild(creerText);
-creerDiv.appendChild(creerImage);
-creerDiv.appendChild(creerText);
-pokemon.appendChild(creerDiv);
-
-fetch("https://pokeapi.co/api/v2/pokemon/").then(response => response.json()).then(data => {
-    // console.log(data);
-    data.results.forEach(pokemon => {
-        fetch(`${pokemon.url}`).then(response => response.json()).then(dataPokemon => {
-            // console.log(dataPokemon);
-            
-            console.log(dataPokemon.id);
-            console.log(dataPokemon.name);
-            console.log(dataPokemon.sprites.front_default);
-            
+for(let i = 0; i<160; i+=20){
+    fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${i}&limit=20`).then(response => response.json()).then(data => {
+        console.log(data.next);
+        data.results.forEach(pokemon => {
+            fetch(`${pokemon.url}`).then(response => response.json()).then(dataPokemon => {
+                console.log(dataPokemon);
+                const pokedex = document.querySelector('.pokedex .conteneur');
+                const pokemon = document.createElement('div');
+    
+                pokemon.className = `${dataPokemon.name}`
+                pokemon.innerHTML = `
+                    <div class="pokemon ${dataPokemon.name}">
+                        <h2 class="nom">${dataPokemon.name}</h2>
+                        <img src="${dataPokemon.sprites.front_default}" alt="Image ${dataPokemon.name}">
+                        <div class="descriptions">
+                            <p class="id">${dataPokemon.id}</p>
+                            <p class="types"></p>
+                        </div>
+                    </div>
+                `;
+                pokedex.appendChild(pokemon);
+                
+                // console.log(dataPokemon.id);
+                // console.log(dataPokemon.name);
+                // console.log(dataPokemon.sprites.front_default);
+            });
         });
     });
-});
+}
