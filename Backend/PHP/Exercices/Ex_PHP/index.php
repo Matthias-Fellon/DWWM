f734adbc0e37cff1eb9120fb48ec472b65a24382
Matthias_F <fellmatt007@gmail.com>
2024-05-13 10:22:36 +0200

    Modifs Dossiers Git

--- Backend/PHP/Exercices/Ex_PHP/index.php
+++ Backend/PHP/Exercices/Ex_PHP/index.php
@@ -1,10 +1,29 @@
 <?php
-$joueur = "Toto";
-$age = 20;
-echo "Nom du joueur : " . $joueur . "\n";
-echo "Age du joueur : " . $age . "\n";
+// $joueur = "Toto";
+// $age = 20;
+// echo "Nom du joueur : " . $joueur . "\n";
+// echo "Age du joueur : " . $age . "\n";
 
 // $age = $age +1;
-$age--;
-echo "Age du joueur : " . $age;
+// $age--;
+// echo "Age du joueur : " . $age;
+
+// string
+$a = "Hello World";
+
+// int
+$b = 1;
+
+// float=double
+$c = 3.14;
+
+// boolean
+$d = false;
+
+echo $a . "\n";
+echo $b . "\n";
+echo $c . "\n";
+echo $d . "\n";
+
+echo gettype($b);
 ?>
