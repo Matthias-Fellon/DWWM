a2d60193c7034d8c3b8cf2f8233596d785927a2c
Matthias_F <fellmatt007@gmail.com>
2024-05-13 09:52:52 +0200

    Exercices PHP

---
+++ Backend/PHP/Exercices/Ex_PHP/index.php
@@ -0,0 +1,10 @@
+<?php
+$joueur = "Toto";
+$age = 20;
+echo "Nom du joueur : " . $joueur . "\n";
+echo "Age du joueur : " . $age . "\n";
+
+// $age = $age +1;
+$age--;
+echo "Age du joueur : " . $age;
+?>
