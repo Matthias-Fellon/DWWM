f76d9b7e2f046edcc142b44af556f717006e3840
Matthias_F <fellmatt007@gmail.com>
2024-05-13 10:33:33 +0200

    Modifs Dossiers Git

--- Backend/PHP/Exercices/Ex_PHP/index.php
+++ Backend/PHP/Exercices/Ex_PHP/index.php
@@ -8,22 +8,25 @@
 // $age--;
 // echo "Age du joueur : " . $age;
 
-// string
-$a = "Hello World";
+// // string
+// $a = "Hello World";
 
-// int
-$b = 1;
+// // int
+// $b = 1;
 
-// float=double
-$c = 3.14;
+// // float=double
+// $c = 3.14;
 
-// boolean
-$d = false;
+// // boolean
+// $d = false;
 
-echo $a . "\n";
-echo $b . "\n";
-echo $c . "\n";
-echo $d . "\n";
+// echo $a . "\n";
+// echo $b . "\n";
+// echo $c . "\n";
+// echo $d . "\n";
 
-echo gettype($b);
+// echo gettype($b);
+
+$operation = 2 + 2 * 5;
+echo $operation;
 ?>
