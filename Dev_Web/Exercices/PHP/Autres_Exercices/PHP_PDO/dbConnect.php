84e133a3f33894f2bb8d95134db7bbacb321ddfa
Matthias_F <fellmatt007@gmail.com>
2024-06-11 16:25:04 +0200

    Exercices PHP

--- Dev_Web/Exercices/PHP/Autres_Exercices/PHP_PDO/dbConnect.php
+++ Dev_Web/Exercices/PHP/Autres_Exercices/PHP_PDO/dbConnect.php
@@ -6,7 +6,7 @@ $user = 'root';
 $pwd = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=$port;dbname$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -19,6 +19,12 @@ try {
     echo "Connexion réussie à la base de données";
 } catch (PDOException $e){
     throw new PDOException($e->getMessage(),(int)$e->getCode()); //Affiche le message d'erreur
+}
+
+$resultat = $pdo->query('SELECT * FROM employe');
+echo "<br>Nom : ";
+foreach ($resultat as $champ) {
+    echo $champ['nom'] . " / "; 
 }
 
 ?>
