f58547c293485afe4b98835f4aea65f0f99ca11f
Matthias_F <fellmatt007@gmail.com>
2024-07-08 16:27:08 +0200

    Projet Chatterie

---
+++ Projets_Fin_de_Formation/Dossier_Professionel/Programmation/Test/Assets/Header/Nav_Bar/index.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Nav Bar</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <section class="top-bar">
+                <div class="back-icon">
+                    <div class="line"></div>
+                    <div class="line"></div>
+                </div>
+            </section>
+            <section class="accordeons">
+                <div class="accordeon">
+                    <button>Accueil</button>
+                    <ul class="panel">
+                        <li><a href="#">Présentation de l'Elevage</a></li>
+                        <li><a href="#">À Propos de Moi</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Actualités</a></li>
+                        <li><a href="#">Galerie Photos/Vidéos</a></li>
+                    </ul>
+                </div>
+                <div class="accordeon">
+                    <button>Elevage</button>
+                    <ul class="panel">
+                        <li><a href="#">Les Mâles</a></li>
+                        <li><a href="#">Les Femelles</a></li>
+                        <li><a href="#">Les Chatons</a></li>
+                        <li><a href="#">Les Retraités</a></li>
+                        <li><a href="#">En Mémoire</a></li>
+                    </ul>
+                </div>
+                <div class="accordeon">
+                    <button>Contact</button>
+                </div>
+                <div class="accordeon">
+                    <button>Conditions de Vente</button>
+                    <ul class="panel">
+                        <li><a href="#">Les Mâles</a></li>
+                        <li><a href="#">Les Femelles</a></li>
+                        <li><a href="#">Les Chatons</a></li>
+                        <li><a href="#">Les Retraités</a></li>
+                        <li><a href="#">En Mémoire</a></li>
+                    </ul>
+                </div>
+                <div class="accordeon">
+                    <button>Galerie</button>
+                    <ul class="panel">
+                        <li><a href="#">Photos</a></li>
+                        <li><a href="#">Videos</a></li>
+                    </ul>
+                </div>
+                <div class="accordeon">
+                    <button>Connexion</button>
+                </div>
+            </section>
+            <img id="imgLogo" src="./Images/Chatterie_des_fell_de_chyme_Black_Logo.png" alt="Logo_du_site">
+        </nav>
+    </header>
+    <script src="./script.js"></script>
+</body>
+</html>
