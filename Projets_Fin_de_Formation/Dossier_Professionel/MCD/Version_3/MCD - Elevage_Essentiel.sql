9feb636590088ed87adb593c523c289576b7d131
Matthias Fellon <fellmatt007@gmail.com>
2024-07-25 17:18:38 +0200

    Projet Chatterie

--- Projets_Fin_de_Formation/Dossier_Professionel/MCD/Version_3/MCD - Elevage_Essentiel.sql
+++ Projets_Fin_de_Formation/Dossier_Professionel/MCD/Version_3/MCD - Elevage_Essentiel.sql
@@ -2,12 +2,113 @@
 #        Script MySQL.
 #------------------------------------------------------------
 
--- Création de la base de données
-DROP DATABASE IF EXIST Chatterie;
-CREATE DATABASE IF NOT EXIST Chatterie
 
--- Utilisation de la base de données
-USE Chatterie;
+#------------------------------------------------------------
+# Table: Vaccin
+#------------------------------------------------------------
+
+CREATE TABLE Vaccin(
+        ID_Vaccin Int  Auto_increment  NOT NULL ,
+        Nom       Varchar (50) NOT NULL
+	,CONSTRAINT Vaccin_PK PRIMARY KEY (ID_Vaccin)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Test
+#------------------------------------------------------------
+
+CREATE TABLE Test(
+        ID_Test Int  Auto_increment  NOT NULL ,
+        Nom     Varchar (50) NOT NULL
+	,CONSTRAINT Test_PK PRIMARY KEY (ID_Test)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Personne
+#------------------------------------------------------------
+
+CREATE TABLE Personne(
+        ID_Personne Int  Auto_increment  NOT NULL ,
+        Nom         Varchar (50) NOT NULL ,
+        Prenom      Varchar (50) NOT NULL ,
+        Sexe        Char (1) NOT NULL ,
+        Telephone   Varchar (15) NOT NULL ,
+        Email       Varchar (150) NOT NULL
+	,CONSTRAINT Personne_PK PRIMARY KEY (ID_Personne)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Eleveur
+#------------------------------------------------------------
+
+CREATE TABLE Eleveur(
+        ID_Personne  Int NOT NULL ,
+        Nom_Elevage  Varchar (50) NOT NULL ,
+        Numero_Siret Varchar (30) NOT NULL ,
+        Description  Text NOT NULL ,
+        Nom          Varchar (50) NOT NULL ,
+        Prenom       Varchar (50) NOT NULL ,
+        Sexe         Char (1) NOT NULL ,
+        Telephone    Varchar (15) NOT NULL ,
+        Email        Varchar (150) NOT NULL
+	,CONSTRAINT Eleveur_PK PRIMARY KEY (ID_Personne)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Privilege
+#------------------------------------------------------------
+
+CREATE TABLE Privilege(
+        ID_Privilege Int  Auto_increment  NOT NULL ,
+        Role         Varchar (50) NOT NULL
+	,CONSTRAINT Privilege_PK PRIMARY KEY (ID_Privilege)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Utilisateur
+#------------------------------------------------------------
+
+CREATE TABLE Utilisateur(
+        ID_Personne        Int NOT NULL ,
+        Mot_De_Passe       Varchar (255) NOT NULL ,
+        Image_Profil       Varchar (50) NOT NULL ,
+        Derniere_Connexion TimeStamp NOT NULL ,
+        Nom                Varchar (50) NOT NULL ,
+        Prenom             Varchar (50) NOT NULL ,
+        Sexe               Char (1) NOT NULL ,
+        Telephone          Varchar (15) NOT NULL ,
+        Email              Varchar (150) NOT NULL ,
+        ID_Privilege       Int NOT NULL
+	,CONSTRAINT Utilisateur_PK PRIMARY KEY (ID_Personne)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Titre
+#------------------------------------------------------------
+
+CREATE TABLE Titre(
+        ID_Titre Int  Auto_increment  NOT NULL ,
+        Nom      Varchar (30) NOT NULL
+	,CONSTRAINT Titre_PK PRIMARY KEY (ID_Titre)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Image
+#------------------------------------------------------------
+
+CREATE TABLE Image(
+        ID_Image Int  Auto_increment  NOT NULL ,
+        Nom      Varchar (50) NOT NULL
+	,CONSTRAINT Image_PK PRIMARY KEY (ID_Image)
+)ENGINE=InnoDB;
+
 
 #------------------------------------------------------------
 # Table: Chat
@@ -29,8 +130,8 @@ CREATE TABLE Chat(
         Livre_Origine       Varchar (30) NOT NULL ,
         ADN                 Varchar (30) NOT NULL ,
         Statut              Varchar (30) NOT NULL ,
-        Date_Adoption       Date ,
         Etat                Varchar (30) NOT NULL ,
+        Date_Adoption       Date ,
         ID_Chat_A_Pour_Mere Int ,
         ID_Chat_A_Pour_Pere Int ,
         ID_Personne         Int NOT NULL
@@ -80,113 +181,8 @@ CREATE TABLE Chat_Titre(
 
 CREATE TABLE Chat_Image(
         ID_Image Int NOT NULL ,
-        ID_Chat  Int NOT NULL ,
-        Image    Varchar (50) NOT NULL
+        ID_Chat  Int NOT NULL
 	,CONSTRAINT Chat_Image_PK PRIMARY KEY (ID_Image,ID_Chat)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Test
-#------------------------------------------------------------
-
-CREATE TABLE Test(
-        ID_Test Int  Auto_increment  NOT NULL ,
-        Nom     Varchar (50) NOT NULL
-	,CONSTRAINT Test_PK PRIMARY KEY (ID_Test)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Vaccin
-#------------------------------------------------------------
-
-CREATE TABLE Vaccin(
-        ID_Vaccin Int  Auto_increment  NOT NULL ,
-        Nom       Varchar (50) NOT NULL
-	,CONSTRAINT Vaccin_PK PRIMARY KEY (ID_Vaccin)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Titre
-#------------------------------------------------------------
-
-CREATE TABLE Titre(
-        ID_Titre Int  Auto_increment  NOT NULL ,
-        Nom      Varchar (30) NOT NULL
-	,CONSTRAINT Titre_PK PRIMARY KEY (ID_Titre)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Image
-#------------------------------------------------------------
-
-CREATE TABLE Image(
-        ID_Image Int  Auto_increment  NOT NULL
-	,CONSTRAINT Image_PK PRIMARY KEY (ID_Image)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Personne
-#------------------------------------------------------------
-
-CREATE TABLE Personne(
-        ID_Personne Int  Auto_increment  NOT NULL ,
-        Nom         Varchar (50) NOT NULL ,
-        Prenom      Varchar (50) NOT NULL ,
-        Adresse     Varchar (150) NOT NULL ,
-        Telephone   Varchar (15) NOT NULL ,
-        Email       Varchar (150) NOT NULL
-	,CONSTRAINT Personne_PK PRIMARY KEY (ID_Personne)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Eleveur
-#------------------------------------------------------------
-
-CREATE TABLE Eleveur(
-        ID_Personne  Int NOT NULL ,
-        Nom_Elevage  Varchar (50) NOT NULL ,
-        Numero_Siret Varchar (30) ,
-        Description  Text NOT NULL ,
-        Nom          Varchar (50) NOT NULL ,
-        Prenom       Varchar (50) NOT NULL ,
-        Adresse      Varchar (150) NOT NULL ,
-        Telephone    Varchar (15) NOT NULL ,
-        Email        Varchar (150) NOT NULL
-	,CONSTRAINT Eleveur_PK PRIMARY KEY (ID_Personne)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Utilisateur
-#------------------------------------------------------------
-
-CREATE TABLE Utilisateur(
-        ID_Personne  Int NOT NULL ,
-        Mot_De_Passe Varchar (255) NOT NULL ,
-        Nom          Varchar (50) NOT NULL ,
-        Prenom       Varchar (50) NOT NULL ,
-        Adresse      Varchar (150) NOT NULL ,
-        Telephone    Varchar (15) NOT NULL ,
-        Email        Varchar (150) NOT NULL ,
-        ID_Privilege Int NOT NULL
-	,CONSTRAINT Utilisateur_PK PRIMARY KEY (ID_Personne)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Privilege
-#------------------------------------------------------------
-
-CREATE TABLE Privilege(
-        ID_Privilege Int  Auto_increment  NOT NULL ,
-        Nom          Varchar (50) NOT NULL
-	,CONSTRAINT Privilege_PK PRIMARY KEY (ID_Privilege)
 )ENGINE=InnoDB;
 
 
